fe1ed5d58eb9b7f576cb3cc0e289421d4375af4e
Masanori Beppu <yaaman01@mbp-mb.local>
2020-08-26 21:52:45 +0900

    if_kadai_saichare3

--- if_kadai.rb
+++ if_kadai.rb
@@ -11,26 +11,17 @@ plans.each.with_index(1) do |plan,i|
 end
 
 choice = gets.to_i
-select_plan = plans[choice-1][:price]
+select_plan = plans[choice-1]
 
-
-case choice
-when 1
-    puts "#{plans[0][:travel]}ですね、何人で行きますか？"
-when 2
-    puts "#{plans[1][:travel]}ですね、何人で行きますか？"
-when 3
-    puts "#{plans[2][:travel]}ですね、何人で行きますか？"
-end
+puts "#{select_plan[:travel]}ですね、何人で行きますか？"
 
 num = gets.to_i
-total_price = select_plan * num
+total_price = select_plan[:price] * num
 
 if num <= 4
-    select_plan * num
     puts "合計金額：¥#{total_price} "
 else
     puts "5人以上なので10%割引となります。"
-    puts "合計金額：¥#{select_plan * num * 0.9} "
+    puts "合計金額：¥#{total_price * 0.9} "
 end
 
